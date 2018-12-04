3446a4b6f1183da7467c566c18976ce0acb66ffa
lxy0828 <741040868@qq.com>
2018-12-04 20:57:11 +0800

    11

---
+++ app/router/routeMap.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Router, Route, IndexRoute } from 'react-router'
+
+import App from '../containers'
+import Home from '../containers/Home'
+import City from '../containers/City'
+import User from '../containers/User'
+import Search from '../containers/Search'
+import Detail from '../containers/Detail'
+import NotFound from '../containers/404'
+
+// 如果是大型项目，router部分就需要做更加复杂的配置
+//最外层App，一个主文件index.jsx，默认得IndexRoute Home,
+class RouterMap extends React.Component {
+    render() {
+        return (
+            <Router history={this.props.history}>
+                <Route path='/' component={App}>
+                    <IndexRoute component={Home}/>//
+                    <Route path='/city' component={City}/>
+                    <Route path='/User' component={User}/>
+                    <Route path='/search/:type(/:keyword)' component={Search}/>
+                    <Route path='/detail/:id' component={Detail}/>
+                    <Route path='*' component={NotFound}/>
+                </Route>
+            </Router>
+        )
+    }
+}
+
+export default RouterMap
